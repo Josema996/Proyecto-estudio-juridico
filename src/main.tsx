dff21045c9965aef79d9da136d41f34218d809cd
Josema <josemariaalmendro@dsuiza.com.ar>
2026-04-20 16:24:17 -0300

    cambios 5

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,10 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+
+// Oculta el splash una vez que React montó
+requestAnimationFrame(() => {
+  setTimeout(() => {
+    (window as Window & { __hideSplash?: () => void }).__hideSplash?.()
+  }, 800)
+})
